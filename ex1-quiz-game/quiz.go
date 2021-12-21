3947856de4446c4ae4f069b1b2861d6c205863b3
Quinn Cheong <quinncheong@hotmail.com>
2021-12-21 20:59:38 +0800

    Add quiz go features with flag package

--- ex1-quiz-game/quiz.go
+++ ex1-quiz-game/quiz.go
@@ -1,9 +1,24 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil { 
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+		os.Exit(1)
+	}
+	_ = file
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
