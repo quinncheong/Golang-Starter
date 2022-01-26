0cf97df57d054626febfee6bc1824a9c33948beb
Quinn Cheong <quinncheong@hotmail.com>
2022-01-26 15:10:10 +0800

    Add some stuff to ex-1

--- ex1-quiz-game/quiz.go
+++ ex1-quiz-game/quiz.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -11,11 +12,47 @@ func main() {
 	flag.Parse()
 
 	file, err := os.Open(*csvFilename)
-	if err != nil { 
+	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
 		os.Exit(1)
 	}
-	_ = file
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+
+	if err != nil {
+		exit(fmt.Sprintf("Failed to parse the provided CSV file: %s\n", *csvFilename))
+		os.Exit(1)
+	}
+
+	problems := parseLines(lines)
+
+	for index, problem := range problems {
+		fmt.Printf("Problem #%d: %s = \n", index+1, problem.question)
+		var answer string
+		fmt.Scanf("%s/n", &answer)
+		fmt.Printf("You answer is: %v\n", answer)
+	}
+
+	// fmt.Println(lines)
+	// fmt.Println(problems)
+}
+
+func parseLines(lines [][]string) []Problem {
+	ret := make([]Problem, len(lines))
+	for i, line := range lines {
+		ret[i] = Problem{
+			question: line[0],
+			answer:   line[1],
+		}
+	}
+
+	return ret
+}
+
+type Problem struct {
+	question string
+	answer   string
 }
 
 func exit(msg string) {
